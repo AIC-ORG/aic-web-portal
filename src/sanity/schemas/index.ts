8181850fc82b78af3a0b50eba22d8af9c427cae3
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-10-04 07:58:54 +0200

    feat: web content

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -3,5 +3,8 @@ import banner from './documents/banner';
 import event from './documents/event';
 import gallery from './documents/gallery';
 import music from './documents/music';
+import webContent from './documents/web-content';
+import bio from './objects/bio';
+import hero from './objects/hero';
 
-export const schemaTypes = [music, album, banner, gallery, event];
+export const schemaTypes = [music, album, banner, gallery, event, webContent, bio, hero];
