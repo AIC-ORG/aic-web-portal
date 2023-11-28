6f193573298ef01603dfc5534ce300cb44b1afda
frank-shema <shemafranko0987@gmail.com>
2023-11-28 15:42:56 +0200

    near final fix

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,50 +1,48 @@
-'use client'
-import * as React from 'react'
-import hotToast, { Toaster as HotToaster } from 'react-hot-toast'
-import { cn } from '@/utils/cn'
+'use client';
+import * as React from 'react';
+import hotToast, { Toaster as HotToaster } from 'react-hot-toast';
+import { cn } from '@/utils/cn';
 
-export const Toaster = HotToaster
+export const Toaster = HotToaster;
 
 interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {
-  visible: boolean
+  visible: boolean;
 }
 
 export function Toast({ visible, className, ...props }: ToastProps) {
   return (
     <div
-      className= {cn('min-h-16 mb-2 flex w-[350px] flex-col items-start gap-1 rounded-md bg-white px-6 py-4 shadow-lg',visible && 'animate-in slide-in-from-bottom-5',
-       className
+      className={cn(
+        'min-h-16 mb-2 flex w-[350px] flex-col items-start gap-1 rounded-md bg-white px-6 py-4 shadow-lg',
+        visible && 'animate-in slide-in-from-bottom-5',
+        className,
       )}
       {...props}
     />
-  )
+  );
 }
 
 interface ToastTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
 
 Toast.Title = function ToastTitle({ className, ...props }: ToastTitleProps) {
-  return <p className={cn('text-sm font-medium', className)} {...props} />
-}
+  return <p className={cn('text-sm font-medium', className)} {...props} />;
+};
 
-interface ToastDescriptionProps
-  extends React.HTMLAttributes<HTMLParagraphElement> {}
+interface ToastDescriptionProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 
-Toast.Description = function ToastDescription({
-  className,
-  ...props
-}: ToastDescriptionProps) {
-  return <p className={cn('text-sm opacity-80', className)} {...props} />
-}
+Toast.Description = function ToastDescription({ className, ...props }: ToastDescriptionProps) {
+  return <p className={cn('text-sm opacity-80', className)} {...props} />;
+};
 
 interface ToastOpts {
-  title?: string
-  message: string
-  type?: 'success' | 'error' | 'default'
-  duration?: number
+  title?: string;
+  message: string;
+  type?: 'success' | 'error' | 'default';
+  duration?: number;
 }
 
 export function toast(opts: ToastOpts) {
-  const { title, message, type = 'default', duration = 3000 } = opts
+  const { title, message, type = 'default', duration = 3000 } = opts;
 
   return hotToast.custom(
     ({ visible }) => (
@@ -53,11 +51,12 @@ export function toast(opts: ToastOpts) {
         className={cn({
           'bg-red-600 text-white': type === 'error',
           'bg-black text-white': type === 'success',
-        })}>
+        })}
+      >
         <Toast.Title>{title}</Toast.Title>
         {message && <Toast.Description>{message}</Toast.Description>}
       </Toast>
     ),
-    { duration }
-  )
+    { duration },
+  );
 }
